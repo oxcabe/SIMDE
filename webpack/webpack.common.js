1276484a4a0bdbd06eb34efa8927306774b202cb
Óscar Carrasco <oxcabe@gmail.com>
2022-04-18 10:50:06 +0100

    Update webpack 'dev' and 'common' config files

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,44 +1,40 @@
-const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = function (options) {
-   return {
-      name: 'app',
-      target: 'web',
-      entry: './src/main.tsx',
-      resolve: {
-         extensions: ['.js', '.ts', '.jsx', '.tsx'],
-      },
-      module: {
-         // loaders: [
-         //    {
-         //          test: /\.ts$/,
-         //          enforce: 'pre',
-         //          loader: 'tslint-loader',
-         //          // options: { 
-         //          //       typeCheck: true
-         //          // }
-         //    },
-         //    { test: /\.tsx?$/, loader: "ts-loader" },
-         //    { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-         //    {
-         //       test: /\.(eot|svg|ttf|woff|woff2)$/,
-         //       loader: 'file-loader?name=public/fonts/[name].[ext]'
-         //    }
-         // ],
-         rules: [
-              {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-              },
-        ]
-      },
-      plugins: [
-         new MiniCssExtractPlugin({
-            filename: '[name].[hash:8].css',
-         })
-      ]
-   }
+module.exports = {
+    entry: {
+        app: './src/main.tsx',
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    },
+    module: {
+       rules: [
+          {
+                test: /\.ts$/,
+                enforce: 'pre',
+                loader: 'tslint-loader',
+                // options: { 
+                //       typeCheck: true
+                // }
+          },
+          { test: /\.css$/i, loader: "css-loader" },
+          { test: /\.tsx?$/, loader: "ts-loader" },
+          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+          {
+            test: /\.(eot|svg|ttf|woff|woff2)$/,
+            loader: 'file-loader',
+
+            options: {
+                name: "public/fonts/[name].[ext]",
+            },
+          },
+       ],
+    },
+    plugins: [
+       new MiniCssExtractPlugin({
+          filename: '[name].[hash:8].css',
+       })
+    ]
 };
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,42 +1,34 @@
-const webpack = require('webpack');
 const path = require('path');
-const commonConfig = require('./webpack.common.js');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const webpackMerge = require('webpack-merge');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
-const ENV = 'dev';
 
-
-module.exports = webpackMerge(commonConfig({ env: ENV }), {
-      devServer: {
-            contentBase: './target/www',
-            historyApiFallback: true
-      },
-      devtool: "source-map",
-      output: {
-            path: path.resolve('target/www'),
-            filename: '[name].bundle.js',
-            chunkFilename: '[id].chunk.js'
-      },
-      plugins: [
-            new BrowserSyncPlugin({
-                  host: 'localhost',
-                  port: 9000,
-                  proxy: {
-                        target: 'http://localhost:9060'
-                  }
-            }, {
-                        reload: true
-                  }),
-            new StyleLintPlugin(),
-            new HtmlWebpackPlugin({
-                  title: 'Simde DEMO',
-                  template: 'src/index.html'
-            }),
-            new CopyWebpackPlugin([
-                  { from: 'src/i18n' }
-            ])
-      ]
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: "source-map",
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './../target/www'),
+        },
+        compress: true,
+        port:9060,
+    },
+    output: {
+          path: path.resolve('target/www'),
+          filename: '[name].bundle.js',
+          chunkFilename: '[id].chunk.js'
+    },
+    plugins: [
+          new StyleLintPlugin(),
+          new HtmlWebpackPlugin({
+                title: 'SIMDE (Development mode)',
+                template: 'src/index.html'
+          }),
+          new CopyWebpackPlugin([
+                { from: 'src/i18n' }
+          ])
+    ]
 });
