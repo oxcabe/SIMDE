2fd1d3042f9f0a7de6e0c0a4eaf57061b1331620
Óscar Carrasco <oxcabe@gmail.com>
2022-09-26 21:59:03 +0100

    Fix dist bundle

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,44 +2,44 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ENV = 'production';
 
-module.exports = webpackMerge(commonConfig({ env: ENV }), {
-      mode: 'development',
-   output: {
-      path: path.resolve("./dist"),
-      filename: "bundle.js"
-   },
-   plugins: [
-      new HtmlWebpackPlugin({
-         title: 'SIMDE - Superescalar Machine Simulator',
-         template: 'src/index.html',
-         inject: true,
-         minify: {
-            removeComments: true,
-            collapseWhitespace: true,
-            removeRedundantAttributes: true,
-            useShortDoctype: true,
-            removeEmptyAttributes: true,
-            removeStyleLinkTypeAttributes: true,
-            keepClosingSlash: true,
-            minifyJS: true,
-            minifyCSS: true,
-            minifyURLs: true
-         }
-      }),
-      new webpack.DefinePlugin({
-         'process.env': {
-            'NODE_ENV': JSON.stringify('production')
-         }
-      }),
-      new webpack.optimize.UglifyJsPlugin(),
-      new CopyWebpackPlugin([
-         { from: 'src/i18n' }
-      ])
-   ],
-});
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+       path: path.resolve("./dist"),
+       filename: "bundle.js"
+    },
+    plugins: [
+       new HtmlWebpackPlugin({
+          title: 'SIMDE - Superescalar Machine Simulator',
+          template: 'src/index.html',
+          inject: true,
+          minify: {
+             removeComments: true,
+             collapseWhitespace: true,
+             removeRedundantAttributes: true,
+             useShortDoctype: true,
+             removeEmptyAttributes: true,
+             removeStyleLinkTypeAttributes: true,
+             keepClosingSlash: true,
+             minifyJS: true,
+             minifyCSS: true,
+             minifyURLs: true
+          }
+       }),
+       new webpack.DefinePlugin({
+          'process.env': {
+             'NODE_ENV': JSON.stringify('production'),
+             'PUBLIC_URL': '\"/SIMDE\"'
+          }
+       }),
+       new CopyPlugin({
+           patterns: [
+               { from: 'src/i18n' }
+           ]
+       }),
+    ],
+}); 
